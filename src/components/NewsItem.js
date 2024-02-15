bb33a5ceba0cabf26c751a43224425442001c0b3
niravgajera1 <niravgajera@gmail.com>
2024-02-15 18:28:46 +0530

    done

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ const NewsItem =(props)=> {
         return (
             <div className="my-3">
 
-                <div className="card"  >
+                <div className="card card-block"  >
                     <div style={{
                         display: 'flex',
                         justifyContent: 'flex-end',
@@ -15,12 +15,12 @@ const NewsItem =(props)=> {
                     }
                     }><span className="badge rounded-pill bg-dark">{source}</span></div>
 
-                    <img src={imageUrl ? imageUrl : "https://i.insider.com/65c2516c43bb77284ba3f7dd?width=1200&format=jpeg"} className="card-img-top" alt="..." />
+                    <img src={imageUrl ? imageUrl : "https://i.insider.com/65c2516c43bb77284ba3f7dd?width=1200&format=jpeg"} className="card-img-top img-fluid" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
                         <p className="card-text"><small className="text-body-secondary">By {author ? author : "Unknown"} On {date} </small></p>
-                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} target='_blank' className="btn btn-outline-info">Read More</a>
                     </div>
                 </div>
             </div>
